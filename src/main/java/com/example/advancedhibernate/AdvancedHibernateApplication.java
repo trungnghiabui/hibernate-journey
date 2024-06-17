b157674ab0b6c834fd3e44493e6d74fc5937ca4a
nghiabt <trungnghia.bui1.11@gmail.com>
2024-06-18 00:30:20 +0700

    init commit

---
+++ src/main/java/com/example/advancedhibernate/AdvancedHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.advancedhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advancedhibernate/AdvancedHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
